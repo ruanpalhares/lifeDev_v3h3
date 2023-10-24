ea3aa94d5ffb8e9726fa99fc200cceb7676fc993
ruanpalhares <ruanpalhares489@gmail.com>
2023-10-23 22:44:41 -0300

    Aula 2310 - Componente Navbar Routerlink

--- lifedev/src/App.jsx
+++ lifedev/src/App.jsx
@@ -2,18 +2,22 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import About from './pages/about/About'
 import Home from './pages/home/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />}></Route>
             <Route path='/about' element={<About />}></Route>
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </>
   )
---
+++ lifedev/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom"
+import styles from "./Navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <>
+      <NavLink to="/">
+        <span>
+          LifeDev
+        </span>
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default Navbar
