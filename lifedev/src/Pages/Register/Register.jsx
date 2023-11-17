d099cccb2c89a210f3621d883db0e1ec25d882af
ruanpalhares <ruanpalhares489@gmail.com>
2023-11-16 22:41:44 -0300

    Aula 1611 - Conectores do Firebase

--- lifedev/src/Pages/Register/Register.jsx
+++ lifedev/src/Pages/Register/Register.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from 'react'
-
-const [displayName, setDisplayName] = useState('')
-const [emailUsuario, setEmailUsuario] = useState('')
-const [senhaUsuario, setSenhaUsuario] = useState('')
-const [confirmaSenha, setConfirmaSenha] = useState('')
-const [erro, setErro] = useState('')
+import React from 'react'
+import { useState } from 'react'
 
 const Register = () => {
+  const [displayName, setDisplayName] = useState('')
+  const [emailUsuario, setEmailUsuario] = useState('')
+  const [senhaUsuario, setSenhaUsuario] = useState('')
+  const [confirmaSenha, setConfirmaSenha] = useState('')
+  const [erro, setErro] = useState('')
+
   return (
     <div>
       <h1>Cadastro de LifeDevelopers</h1>
       <form>
         <label>
           <span>Nome: </span>
-          <input type="text" name="displayName" placeholder="Digite seu nome de usuário..."  required/>
+          <input type="text" name="displayName" placeholder="Digite seu nome de usuário..." required />
         </label>
         <label>
           <span>E-mail: </span>
-          <input type="email" name="emailUsuario" placeholder="Entre com seu e-mail..."  required/>
+          <input type="email" name="emailUsuario" placeholder="Entre com seu e-mail..." required />
         </label>
         <label>
           <span>Senha: </span>
-          <input type="password" name="senhaUsuario" placeholder="Entre com uma senha segura..."  required/>
+          <input type="password" name="senhaUsuario" placeholder="Entre com uma senha segura..." required />
         </label>
         <label>
           <span>Confirmação: </span>
-          <input type="password" name="confirmaSenha" placeholder="Confirme sua senha..."  required/>
+          <input type="password" name="confirmaSenha" placeholder="Confirme sua senha..." required />
         </label>
         <button className='btn'>Cadastrar</button>
       </form>
