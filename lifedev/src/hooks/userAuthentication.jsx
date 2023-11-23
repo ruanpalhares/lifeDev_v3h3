4a5047bc2c88543d03c8d65da892f2a016a1ee3c
ruanpalhares <ruanpalhares489@gmail.com>
2023-11-23 20:43:57 -0300

    Aula 2311 - Setup do Firebase no FrontEnd

--- lifedev/src/hooks/userAuthentication.jsx
+++ lifedev/src/hooks/userAuthentication.jsx
@@ -13,26 +13,58 @@ export const userAuthentication = () => {
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(null)
     const [cancelled, setCancelled] = useState(false)
-    
+
     const auth = getAuth()
 
-    function checkIfCancelled(){
+    function checkIfCancelled() {
         if (cancelled) return
     }
 
-    async function createUser(data){
+    async function createUser(data) {
         checkIfCancelled()
-        
+
         setLoading(true)
 
         setError(null)
-        
+
         try {
             const { user } = await createUserWithEmailAndPassword(
-                auth, data.email, data.password
+                auth,
+                data.email,
+                data.password,
             )
+
+            await updateProfile(user, {
+                displayName: data.displayName
+            })
+
+            setLoading(false)
+
+            return user
         } catch (error) {
-            
+            console.error(error.message)
+            console.error(typeof error.message)
+
+            let systemErrorMessage
+
+            if (error.message.includes("password")) {
+                systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres."
+            } else if(error.message.includes("email-already")){
+                systemErrorMessage = "Este email já esta cadastrado"
+            } else {
+                systemErrorMessage = "Ocorreu um erro, tente novamente"
+            }
         }
+    }
+
+    useEffect(() => {
+        return () => setCancelled(true)
+    }, [])
+
+    return{
+        auth,
+        createUser,
+        error,
+        loading
     }
 }
